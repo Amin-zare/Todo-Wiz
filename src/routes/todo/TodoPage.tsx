dc608f89fc0c4458c37ab1d67d221020d1081cb0
Amin <zare.mohammadamin@gmail.com>
2023-09-28 16:53:47 +0200

    Add Done/Undone  button to TodoPage

--- src/routes/todo/TodoPage.tsx
+++ src/routes/todo/TodoPage.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import Todo from '../../components/models/Todo'
 
 type TodoItem = {
   id: string
@@ -14,13 +16,26 @@ type TodoParams = {
 
 const TodoPage: React.FC = () => {
   const params = useParams<TodoParams>()
-
   const todos = JSON.parse(localStorage.getItem('TODOS') || '[]') as TodoItem[]
-
-  const todo = todos.find(t => t.id === params.id)
+  const tishTodo = todos.find(t => t.id === params.id)
+  const [todo, setTodo] = useState<Todo | undefined>(tishTodo) // Initialize state
 
   // Getting the status from the matched todo item or defaulting to false
   const status = todo?.is_done || false
+
+  const toggleTodoStatus = (id: string): void => {
+    const updatedTodos = todos.map(todoItem =>
+      todoItem.id === id
+        ? { ...todoItem, is_done: !todoItem.is_done }
+        : todoItem,
+    )
+    const updatedTodo = updatedTodos.find(t => t.id === id)
+
+    localStorage.setItem('TODOS', JSON.stringify(updatedTodos))
+    if (updatedTodo) setTodo(updatedTodo) // Update the single todo state
+    toast('Status changed.')
+  }
+
   return (
     <div className='container mt-5'>
       <div className='card shadow'>
@@ -47,9 +62,22 @@ const TodoPage: React.FC = () => {
           </p>
         </div>
       </div>
-      <Link to='/' className='btn btn-secondary mt-3'>
-        <i className='fa fa-arrow-left'></i> Back to Home
-      </Link>
+      <div className='d-flex justify-content-between align-items-center rounded'>
+        <Link to='/' className='btn btn-secondary mt-3'>
+          <i className='fa fa-arrow-left'></i> Back to Home
+        </Link>
+        <div>
+          <button
+            type='button'
+            className={`btn btn-sm me-1 ${
+              todo?.is_done ? ' btn-secondary' : 'btn-success'
+            } `}
+            onClick={() => toggleTodoStatus(todo!.id!)}
+          >
+            {todo?.is_done ? 'Undone' : 'Done'}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
