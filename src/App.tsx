a41b5da5adbadba9317489ead8bd2f9ffd794b47
Amin <zare.mohammadamin@gmail.com>
2023-09-28 13:22:38 +0200

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Header from './components/layouts/Header'
 import { Toaster } from 'sonner'
 import Home from './routes/Home'
 import { Route, Routes } from 'react-router-dom'
-import About from './routes/About'
-import Contact from './routes/Contact'
-import NotFound from './routes/NotFound'
+import { lazy, Suspense } from 'react'
+
+const About = lazy(() => import('./routes/About'))
+const Contact = lazy(() => import('./routes/Contact'))
+const NotFound = lazy(() => import('./routes/NotFound'))
 
 function App() {
   return (
@@ -15,9 +17,30 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='*' element={<NotFound />} />
+          <Route
+            path='/about'
+            element={
+              <Suspense fallback={<h2>Loading</h2>}>
+                <About />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/Contact'
+            element={
+              <Suspense fallback={<h2>Loading</h2>}>
+                <Contact />
+              </Suspense>
+            }
+          />
+          <Route
+            path='*'
+            element={
+              <Suspense fallback={<h2>Loading</h2>}>
+                <NotFound />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
     </>
